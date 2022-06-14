64fc783e5badc4a8352c8495a7bc420147082947
Vaseem Mohammed <mdwasim@outlook.com>
2022-06-14 16:27:03 +0400

    PyCharm

---
+++ Projects/GUI-Test/classes.py
@@ -0,0 +1,10 @@
+from PySide6.QtWidgets import QApplication, QWidget, QMainWindow
+from PySide6.QtGui import QFont, QIcon
+from PySide6.QtCore import QSize
+
+app = QApplication([])
+
+window = QMainWindow()
+window.show()
+
+app.exec_()
